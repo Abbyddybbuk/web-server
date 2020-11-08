2129fc3aa605f05cc8d13138fe59f06ac377d383
I066841 <a.kulshreshtha@sap.com>
2020-11-08 11:11:34 +0530

    Set-up Static web-pages for your website

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
+const path = require('path')//this is node core-library and does not require any npm install
 const express = require('express')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+// This is to ensure that your node projects boots-up with this folder
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Abhijeet',
-        age: 35
-    },
-    {
-        name: 'Ankur',
-        age: 34
-    }
-    ])
-})
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Abhijeet',
+//         age: 35
+//     },
+//     {
+//         name: 'Ankur',
+//         age: 34
+//     }
+//     ])
+// })
 
-app.get('/about', (req, res) => {
-    res.send('<h1>We provide you weather forecast</h1>')
-})
+// app.get('/about', (req, res) => {
+//     res.send('<h1>We provide you weather forecast</h1>')
+// })
 
 app.get('/weather', (req, res) => {
     res.send([{
